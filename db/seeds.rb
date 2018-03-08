245e6c2c45b0dcab068f866619360f4864534e2b
Matt Sampson <matthew@therubythree.com>
2018-03-08 12:42:11 -0800

    Styled navbar header, removed scaffold.scss, and configured root path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!(
+  title: 'Space Exploration: The Dark Age'
+)
+
+Book.create!(
+  title: 'How to Win Friends and Influence Others'
+)
