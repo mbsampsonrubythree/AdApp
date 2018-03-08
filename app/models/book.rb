de313987d24c71afb6be1e9c829d0359482df48a
Matt Sampson <matthew@therubythree.com>
2018-03-08 15:58:38 -0800

    Created a relationship between Book and Category model (book belongs_to category)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
+  belongs_to :category
   validates_presence_of :title, :description, :image
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Book.create!(
-  title: 'Space Exploration: The Dark Age',
-  image: 'http://via.placeholder.com/100x150',
-  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
-)
-
-Book.create!(
-  title: 'The Journey of a Lifetime',
-  image: 'http://via.placeholder.com/100x150',
-  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
-)
-
-Book.create!(
-  title: 'Hagrid Potter',
-  image: 'http://via.placeholder.com/100x150',
-  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
-)
-
-Book.create!(
-  title: 'Lord of the Trilogy',
-  image: 'http://via.placeholder.com/100x150',
-  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
-)
-
-puts "generated book seeds"
-
 Category.create!(
   name: "General"
 )
@@ -49,3 +23,33 @@ Category.create!(
 )
 
 puts "generated category seeds"
+
+Book.create!(
+  title: 'Space Exploration: The Dark Age',
+  image: 'http://via.placeholder.com/100x150',
+  category_id: 1,
+  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
+)
+
+Book.create!(
+  title: 'The Journey of a Lifetime',
+  image: 'http://via.placeholder.com/100x150',
+  category_id: 1,
+  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
+)
+
+Book.create!(
+  title: 'Hagrid Potter',
+  image: 'http://via.placeholder.com/100x150',
+  category_id: 1,
+  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
+)
+
+Book.create!(
+  title: 'Lord of the Trilogy',
+  image: 'http://via.placeholder.com/100x150',
+  category_id: 1,
+  description: 'Fusce et neque urna. Aenean finibus nibh sit amet elit bibendum, et maximus ante ullamcorper. Mauris mattis quam elit, eu tempor neque euismod eget. Vestibulum risus diam, sodales vitae est eu, dictum bibendum purus. Ut convallis mollis ex in facilisis. Donec tincidunt sed augue at sodales. Aliquam ornare lorem a tristique faucibus. Integer ornare turpis est, at dignissim nunc volutpat ac. Etiam in finibus ex. Aenean posuere eleifend interdum. Nulla ac auctor ante.'
+)
+
+puts "generated book seeds"
